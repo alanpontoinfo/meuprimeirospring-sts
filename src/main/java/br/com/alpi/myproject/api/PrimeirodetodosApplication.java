1a164f8dfc58e1fd56c2a67fc3dcc9d659aabade
Alan Santos <alanpontoinfo@gmail.com>
2019-10-09 12:01:30 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/alpi/myproject/api/PrimeirodetodosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alpi.myproject.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirodetodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirodetodosApplication.class, args);
+	}
+
+}
